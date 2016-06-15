bb249e0027b0664122065c4acc8098784a0407a7
allan <epicallan.al@gmail.com>
2016-06-15 12:54:45 +0300

    error handling errors

--- src/router.js
+++ src/router.js
@@ -7,20 +7,22 @@ var router = express.Router();
 router.get('/', function(req, res) {
   res.send('deploy server is running')
 });
+
 router.post('/deploy', function(req, res) {
     var payload;
-    if (typeof req.body === 'object')
-        {payload = req.body.payload;}
-    else {
+    if (typeof req.body === 'object'){
+      payload = req.body.payload;
+    } else {
         console.log(req.body);
         payload = JSON.parse(req.body);
     }
     _.each (repos, function (repo) {
-        console.log('repository.name: ', payload.repository.name )
+        console.log('repository.name: ', payload.repository.name);
         if (repo.name === payload.repository.name &&
             payload.ref.indexOf(repo.ref) >= 0) {
                 run_cmd(repo.deploy);
-        } res.send('deployed');
+        }
+        res.send('deployed');
     });
 });
 module.exports = router;
